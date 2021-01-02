78898c40051c7bb07227fd3756d9f7c8454fc9b0
DRLimana <daniellimana@gmail.com>
2021-01-02 11:05:23 -0300

    Created auditable entity

--- src/main/java/com/dlimana/bookstoremanager/users/entity/User.java
+++ src/main/java/com/dlimana/bookstoremanager/users/entity/User.java
@@ -1,6 +1,7 @@
 package com.dlimana.bookstoremanager.users.entity;
 
 import com.dlimana.bookstoremanager.books.entity.Book;
+import com.dlimana.bookstoremanager.entity.Auditable;
 import com.dlimana.bookstoremanager.users.enums.Gender;
 import lombok.Data;
 
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class User {
+public class User extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
