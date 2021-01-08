13816e21fef8b161769a6ae3671972a176d0203d
DRLimana <daniellimana@gmail.com>
2021-01-08 15:24:01 -0300

    Created base authors structure classes

---
+++ src/main/java/com/dlimana/bookstoremanager/author/controller/AuthorControllerDocs.java
@@ -0,0 +1,8 @@
+package com.dlimana.bookstoremanager.author.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Authors management")
+public interface AuthorControllerDocs {
+
+}
---
+++ src/main/java/com/dlimana/bookstoremanager/author/mapper/AuthorMapper.java
@@ -0,0 +1,17 @@
+package com.dlimana.bookstoremanager.author.mapper;
+
+import com.dlimana.bookstoremanager.author.dto.AuthorDTO;
+import com.dlimana.bookstoremanager.author.entity.Author;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AuthorMapper {
+
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    Author toModel(AuthorDTO authorDTO);
+
+    AuthorDTO toDTO(Author author);
+
+}
