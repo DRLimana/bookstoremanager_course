8259db5a63c93673adc9691049e25d223e80f8c2
DRLimana <daniellimana@gmail.com>
2021-01-28 16:23:03 -0300

    Developed user creation feature

--- src/main/java/com/dlimana/bookstoremanager/users/service/UserService.java
+++ src/main/java/com/dlimana/bookstoremanager/users/service/UserService.java
@@ -1,9 +1,15 @@
 package com.dlimana.bookstoremanager.users.service;
 
+import com.dlimana.bookstoremanager.users.dto.MessageDTO;
+import com.dlimana.bookstoremanager.users.dto.UserDTO;
+import com.dlimana.bookstoremanager.users.entity.User;
+import com.dlimana.bookstoremanager.users.exception.UserAlreadyExistsException;
 import com.dlimana.bookstoremanager.users.mapper.UserMapper;
 import com.dlimana.bookstoremanager.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +20,28 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public MessageDTO create(UserDTO userToCreateDTO){
+        verifyIfExsits(userToCreateDTO.getEmail(), userToCreateDTO.getUsername());
+        User userToCreate = userMapper.toModel(userToCreateDTO);
+        User createdUser = userRepository.save(userToCreate);
+        return creationMessage(createdUser);
+    }
+
+    private void verifyIfExsits(String email, String username) {
+        Optional<User> foundUser = userRepository.findByEmailOrUsername(email, username);
+        if(foundUser.isPresent()){
+            throw new UserAlreadyExistsException(email, username);
+        }
+    }
+
+    private MessageDTO creationMessage(User createdUser) {
+        String createdUserName = createdUser.getUsername();
+        Long createdId = createdUser.getId();
+        String createdUserMessage = String.format("User %s with ID %s successfully created", createdUserName, createdId);
+        return MessageDTO.builder()
+                .message(createdUserMessage)
+                .build();
     }
 }
