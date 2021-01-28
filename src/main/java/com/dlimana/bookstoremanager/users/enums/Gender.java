b526ec35125c5563ccab02e0ec383162b4d02d2e
DRLimana <daniellimana@gmail.com>
2021-01-28 11:13:44 -0300

    Created User DTO class

--- src/main/java/com/dlimana/bookstoremanager/users/enums/Gender.java
+++ src/main/java/com/dlimana/bookstoremanager/users/enums/Gender.java
@@ -1,13 +1,14 @@
 package com.dlimana.bookstoremanager.users.enums;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum Gender {
 
     MALE("Male"),
     FEMALE("Female");
 
-    Gender(String description) {
-    }
+    private String description;
 }
